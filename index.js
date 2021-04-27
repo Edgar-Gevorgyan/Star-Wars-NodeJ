a5456a89a05e00d5845ee48a96ab6a7e659ac1a9
Edgar <edgar777@outlook.fr>
2021-04-27 21:32:26 +0200

    Improves the computation of the sum

--- index.js
+++ index.js
@@ -10,20 +10,21 @@ const filmID = args.getFilmID()
 
 axios.get(`http://swapi.dev/api/films/${filmID}/`)
 .then((response) => {
-    let planetDiameters = []
     let planetRequests = []
     // sends requests to retrieve one by one the data of each planet present in the movie #${filmID}
     for(const planetURL of response.data.planets){
         let planetRequest = axios.get(planetURL)
         planetRequests.push(planetRequest)
     }
+    let planetsDiameterSum = 0
     axios.all(planetRequests).then((planetResponses) => { // wait until all the data of the plants are retrieved
         for(const planetResponse of planetResponses){
             let planet = new Planet(planetResponse.data)
-            if(planet.isValid()) planetDiameters.push(planet.getDiameter())
+            // take account in the sum only if the planet respects the conditions
+            if(planet.isValid()) planetsDiameterSum += planet.getDiameter()
         }
         // displays the sum of the diameters of the valid planets
-        console.log(planetDiameters.reduce((a, b) => a + b, 0))
+        console.log(planetsDiameterSum)
     }).catch((err) => {
         errors.showMessage(err, "An internal error occured, try again :'(")
     })
